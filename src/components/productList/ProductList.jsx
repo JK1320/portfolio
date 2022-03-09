ea8e4fae526317239e9bcbedbda98e9bb58da4b2
JK1320 <kaurjus1@gmail.com>
2022-03-09 17:45:42 +0000

    added projects

--- src/components/productList/ProductList.jsx
+++ src/components/productList/ProductList.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import "./productList.css";
+import Product from "../product/Product";
+import {projects} from "../../data";
 
+console.log(projects)
 const ProductList = () => {
     return (
-        <div>
-            
+        <div className="pl">
+            <div className="pl-texts">
+                <h1 className="pl-title">Create & Inspire</h1>
+                <p className="pl-desc">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus quas voluptates dolore distinctio praesentium magnam nobis! Amet repellat accusamus numquam sed. Quod iste impedit velit quasi nam eius perferendis aliquam!
+                </p>
+            </div>
+            <div className="pl-list">
+                {projects.map((item) => (
+                    <Product key={item.id} img={item.img} link={item.link} />
+    ))}  
+            </div>
         </div>
     )
 }
---
+++ src/data.js
@@ -0,0 +1,55 @@
+export const projects = [
+  {
+    id: 1,
+    project: "Baby Names",
+    img: "./projectsImages/babyNames.jpg",
+    link: "https://cyf-jk1320-baby-names.netlify.app/",
+    github: "https://github.com/JK1320/baby-names-react",
+    info: "You can find a baby name",
+  },
+
+  {
+    id: 2,
+    project: "Countries",
+    img: "./projectsImages/countries.jpg",
+    link: "https://cyf-jk1320-countries-react.netlify.app/",
+    github: "https://github.com/JK1320/countries-react",
+    info: "hello",
+  },
+
+  {
+    id: 3,
+    project: "Jobs Listings",
+    img: "./projectsImages/jobListings.jpg",
+    link: "https://cyf-jk1320-jobs.netlify.app/",
+    github: "https://github.com/JK1320/job-listings-react",
+    info: "hello",
+  },
+
+  {
+    id: 4,
+    project: "Knowledge Checklist",
+    img: "./projectsImages/KnowledgeChecklist.jpg",
+    link: "cyf-knowledge-checklists.herokuapp.com/",
+    github: "https://github.com/aaokunade/Cyf-Knowledge-CheckList",
+    info: "This was the final project completed in a team of four.",
+  },
+
+  {
+    id: 5,
+    project: "Knowledge Checklist",
+    img: "./projectsImages/KnowledgeChecklist.jpg",
+    link: "cyf-knowledge-checklists.herokuapp.com/",
+    github: "https://github.com/aaokunade/Cyf-Knowledge-CheckList",
+    info: "This was the final project completed in a team of four.",
+  },
+
+  {
+    id: 6,
+    project: "Knowledge Checklist",
+    img: "./projectsImages/KnowledgeChecklist.jpg",
+    link: "cyf-knowledge-checklists.herokuapp.com/",
+    github: "https://github.com/aaokunade/Cyf-Knowledge-CheckList",
+    info: "This was the final project completed in a team of four.",
+  },
+];
