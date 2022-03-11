677e90ab2760ef27202f1253451287afa829716c
JK1320 <kaurjus1@gmail.com>
2022-03-11 16:24:28 +0000

    added toggle folder

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -27,6 +27,7 @@ const Contact = () => {
           (result) => {
             console.log(result.text);
             // setDone(true);
+            // alert("Thanks for the message")
             e.target.reset();
           },
           (error) => {
