1f18cab11865c05d60dd8876710a2796538618bd
JK1320 <kaurjus1@gmail.com>
2022-03-12 22:02:53 +0000

    added media query to productList

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useRef, useState } from 'react';
+import React, { useContext, useRef } from 'react';
 import "./contact.css";
 import emailjs from 'emailjs-com';
 import { ThemeContext } from '../../context';
