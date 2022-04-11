a4af1b844819ac0411c4caea345e78459c9e835c
JK1320 <kaurjus1@gmail.com>
2022-04-11 23:36:44 +0100

    added text in about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -8,39 +8,33 @@ const About = () => {
         <div className="a-left">
           <div className="a-card bg"></div>
           <div className="a-card">
-            <img
-              src={Award}
-              alt=""
-              className="a-img"
-            />
+            <img src={Award} alt="" className="a-img" />
           </div>
         </div>
         <div className="a-right">
           <h1 className="a-title">About Me</h1>
           <p className="a-sub">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut, iusto
-            amet, sapiente doloribus dignissimos voluptatum iure dolor dolorum
-            voluptates accusantium at? Quis velit libero laudantium praesentium
-            sint. Accusamus, nostrum laborum?
+            I had left my job due to child care responsibilities in 2019. I
+            started looking into a career in tech. I was not quite sure which
+            path to take as tech industry is so vast.
           </p>
           <p className="a-desc">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro nemo
-            officiis quis aut id ex dignissimos, molestias modi exercitationem,
-            sapiente ab, similique veniam soluta? Vel quas corporis tenetur eos
-            nisi?
+            I came across CodeYourFuture, who took me onto their programme to
+            train me towards becoming a full stack developer.
           </p>
           <div className="a-award">
             {/* <img src={Award} alt="" className="a-award-img" /> */}
             <div className="a-award-texts">
               <h4 className="a-award-title">Full Stack Developer</h4>
               <p className="a-award-desc">
-                I have graduated as a full stack developer from CodeYourFuture. My final project was 
-                Knowledge Checklist, a Full Stack Project. I worked in a team of 4
-                to create an app in 4 weeks. I contributed to the front-end
-                (created wireframe and react components) and the back-end
-                (contributed to creating routes, tables in database & writing
-                sql queries) aspects of the app. We used React, Node, Express
-                and PostgreSQL, CSS to create it and used heroku to deploy it.
+                I have graduated as a full stack developer from CodeYourFuture.
+                My final project was Knowledge Checklist, a Full Stack Project.
+                I worked in a team of 4 to create an app in 4 weeks. I
+                contributed to the front-end (created wireframe and react
+                components) and the back-end (contributed to creating routes,
+                tables in database & writing sql queries) aspects of the app. We
+                used React, Node, Express and PostgreSQL, CSS to create it and
+                used heroku to deploy it.
               </p>
             </div>
           </div>
